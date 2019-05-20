5a9ddb6a2da3e88643582b6f512051e0391d907e
Jeroen Vermazeren <jeroenvermazeren@outlook.com>
2019-05-20 16:50:41 +0200

    Update apply_acor.py

--- ACO_continuous/apply_acor.py
+++ ACO_continuous/apply_acor.py
@@ -7,22 +7,23 @@ Simple example of how to apply the ACOr class.
 
 import ant_colony_for_continuous_domains
 import continuous_benchmarks
-from ctf.functions2d import eggholder, three_hump_camel
+from ctf.functionsnd import rosenbrock, rastrigin, sphere, styblinski_tang
 from scipy.optimize import rosen
 
 colony = ant_colony_for_continuous_domains.ACOr()
-ranges = [[-512,512],
-          [-512,512]]
+ranges = [[-512,512],[-512,512],[-512,512],[-512,512],[-512,512],[-512,512],[-512,512],[-512,512],[-512,512], [-512,512]]
 
-functions = [eggholder.Eggholder().cost, three_hump_camel.ThreeHumpCamel().cost, rosen]
-
+functions = [rosenbrock.Rosenbrock(10), rastrigin.Rastrigin(10), sphere.Sphere(10), styblinski_tang.StyblinskiTang(10)] 
 for function in functions:
-
-	colony.set_cost(function)
-	colony.set_variables(2, ranges)
+	colony.set_cost(function.cost)
+	colony.set_variables(10, ranges)
 	colony.set_parameters(500, 50, 50, 0.0001, 0.5)
 	# colony.set_parameters(100, 5, 50, 0.01, 0.85)
 
 	solution = colony.optimize()
-
-	print(solution[0:2])
+	print(function.latex_name)
+	print(function.latex_desc)
+	print('minimum of the function', str(function.min))
+	print('best found solution:')
+	print(solution[0:10])
+	print()
